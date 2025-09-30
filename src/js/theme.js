64eef6b2b79fb2d332a5b55534adbfcc3315e08c
TheArtistWeb <teamtheartistweb@gmail.com>
2025-09-30 11:42:49 +0300

    edit

---
+++ src/js/theme.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * Import
+ */
+import { addEventsOnElements } from "./utils.js";
+
+/* Elements */
+const $htmlTheme = document.documentElement;
+
+const toggleTheme = () => {
+    const currentTheme = $htmlTheme.getAttribute("data-theme") || "light";
+    const newTheme = currentTheme === "light" ? "dark" : "light";
+
+    $htmlTheme.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+};
+
+/**
+ * Initialize the theme
+ */
+const storedTheme = localStorage.getItem("theme");
+const systemThemeIsDark = window.matchMedia("prefers-color-theme: dark").matches;
+const initializeTheme = storedTheme ?? (systemThemeIsDark ? "dark" : "light");
+
+$htmlTheme.setAttribute("data-theme", initializeTheme);
+
+window.addEventListener("DOMContentLoaded", () => {
+    const $toggleThemes = document.querySelectorAll("[data-toggle-theme]");
+
+    addEventsOnElements($toggleThemes, "click", toggleTheme);
+});
